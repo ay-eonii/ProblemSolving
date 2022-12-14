755aee71137bf154dcb0fe6c460d3c1bef6d9318
ay-eonii <naayen@naver.com>
2022-12-14 14:55:00 +0900

    Updated 2022.12.14

--- 최대값 만들기(1).java
+++ 최대값 만들기(1).java
@@ -1,40 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max1=numbers[0];
-        int sec=numbers[0];
+			int size = numbers.length;
+			Arrays.sort(numbers);
 
-        int flag = 0;
-
-        for (int n : numbers) {
-            if (max1<n) {
-							max1 = n;
-						}
-        }
-        // 
-        for (int num : numbers) {
-            if (num<max1 && sec<num) {
-							sec = num;
-						}
-            if (num==max1) {
-							flag++;
-						}
-					if (flag>=2) {
-						sec = max1;
-					}
-        }
-        return max1 * sec;
+			return numbers[size - 1] * numbers[size - 2];
     }
-
-
-		public void main(String[] args) {
-			int[] nums = {30,25,4,3,2};
-
-			System.out.println(solution(nums));
-		}
 }
-
-        // System.out.println(max1);
-        // System.out.println(sec);
-        // int max1 = 0;
-        // int sec = 0;
-
