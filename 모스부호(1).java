e59cd4b42be8768a900f1b0c3c67d7e6e69586d1
ay-eonii <naayen@naver.com>
2022-12-19 22:00:59 +0900

    Updated 2022.12.19

---
+++ 모스부호(1).java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public String solution(String letter) {
+    String[] morse = {
+      ".-",
+      "-...",
+      "-.-.",
+      "-..",
+      ".",
+      "..-.",
+      "--.",
+      "....",
+      "..",
+      ".---",
+      "-.-",
+      ".-..",
+      "--",
+      "-.",
+      "---",
+      ".--.",
+      "--.-",
+      ".-.",
+      "...",
+      "-",
+      "..-",
+      "...-",
+      ".--",
+      "-..-",
+      "-.--",
+      "--..",
+    };
+    String[] alphabet = {
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    };
+
+    String[] letterArray = letter.split(" ");
+    String answer = "";
+    int index = 0;
+
+    for (String str : letterArray) {
+      index = Arrays.asList(morse).indexOf(str);
+      answer += alphabet[index];
+    }
+
+    return answer;
+  }
+}
