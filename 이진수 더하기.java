1a2fad8091423ee51ba195a3b5bdcf6f69460bf7
ටㅏ연 <110026001+ay-eonii@users.noreply.github.com>
2022-12-24 23:34:18 +0900

    Updated 2022.12.24

--- 이진수 더하기.java
+++ 이진수 더하기.java
@@ -34,7 +34,7 @@ class Solution {
   }
 }
 
-// 다른 사람 풀이
+// 다 풀이
 class Solution {
     public String solution(String bin1, String bin2) {
         return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2),2);
