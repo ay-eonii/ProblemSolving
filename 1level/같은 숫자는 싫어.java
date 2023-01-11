059bff21db3120a61f0e791e6abe43f092bf863f
ay-eonii <naayen@naver.com>
2023-01-11 13:41:13 +0900

    Updated 2023.01.11

---
+++ 1level/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+  public int[] solution(int[] arr) {
+    Stack<Integer> st = new Stack<>();
+    st.push(arr[0]);
+    for (int i : arr) {
+      if (st.peek() != i) {
+        st.push(i);
+      }
+    }
+    return st.stream().mapToInt(i -> i).toArray();
+  }
+}
