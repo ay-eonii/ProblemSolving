807f94a7285bc2ce96b7361aa7410ba46d423059
ay-eonii <naayen@naver.com>
2023-01-27 22:06:07 +0900

    Updated 2023.01.27

--- programmers/1level/제일 작은 수 제거하기.java
+++ programmers/1level/제일 작은 수 제거하기.java
@@ -1,14 +1,19 @@
+/* problem
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 
+예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+ */
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length == 1) {
-            return new int[]{-1};
-        } 
-        
+            return new int[] { -1 };
+        }
+
         int min = Integer.MAX_VALUE;
         for (int i : arr) {
-            min = min > i ? i : min; 
+            min = min > i ? i : min;
         }
-        
+
         int[] answer = new int[arr.length - 1];
         int index = 0;
         for (int i : arr) {
@@ -21,7 +26,8 @@ class Solution {
 }
 
 /*
-1. 최소값
-2. arr-1 길이 배열에 저장
-3. arr==1인 경우 return [-1];
-*/
+ * strategy
+ * 1. 최소값
+ * 2. arr-1 길이 배열에 저장
+ * 3. arr==1인 경우 return [-1];
+ */
