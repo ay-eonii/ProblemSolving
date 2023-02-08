b6fa161011224e804407f2a32532a577254afdc4
ay-eonii <naayen@naver.com>
2023-02-08 14:24:38 +0900

    Updated 2023.02.08

--- programmers/0level/평행.java
+++ programmers/0level/평행.java
@@ -4,7 +4,7 @@
 주어진 네 개의 점을 두 개씩 이었을 때, 두 직선이 평행이 되는 경우가 있으면 1을 없으면 0을 return 하도록 solution 함수를 완성해보세요.
  */
 
-class Solution {
+class Solution1 {
     public int solution(int[][] dots) {
         float[] distances = new float[2];
         float[] temp = new float[2];
@@ -31,7 +31,24 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public int solution(int[][] dots) {
+        if (gradient(dots[0], dots[1]) == gradient(dots[2], dots[3])) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public float gradient(int[] a, int[] b) {
+        float top = Math.abs(a[0] - b[0]);
+        float bot = Math.abs(a[1] - b[1]);
+        return top / bot;
+    }
+}
+
 /*
+ * strategy
  * 0. dots 순회하면서 판단
  * 1. 평행 조건: 두 선분의 기울기 같아야 함 -> x의 차, y의 차 같아야 함 && 부호도 같아야함
  * 2. 두 선분 만드는 경우의 수: [0]&[1]~[2]&[3] || [0]&[2]~[1]&[3] || [0]&[3]~[1]&[2]
