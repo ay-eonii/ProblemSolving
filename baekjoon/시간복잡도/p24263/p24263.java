5f2a241393cca5595dba2dcef552e2c13acc9ad9
ay-eonii <naayen@naver.com>
2023-03-02 19:06:30 +0900

    Updated 2023.03.02

---
+++ baekjoon/시간복잡도/p24263/p24263.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p24263 {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int n = stdin.nextInt();
+
+        System.out.println(n);
+        System.out.println("1");
+    }
+}
