b89535982984ebe9b8766a379dcb4db42252b753
ay-eonii <naayen@naver.com>
2023-03-03 23:03:15 +0900

    Updated 2023.03.03

--- baekjoon/timecomplexity/p24267/src/p24267.java
+++ baekjoon/timecomplexity/p24267/src/p24267.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+package baekjoon.timecomplexity.p24267.src;
+
+import java.util.Scanner;
+
+public class p24267 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Long n = scanner.nextLong();
+        System.out.println(n * (n - 1) * (n - 2) / 6);
+        System.out.println("3");
+        scanner.close();
     }
 }
