4d2c100a6f8f382f2808410585b1681d00cd6513
ay-eonii <naayen@naver.com>
2023-03-15 22:05:20 +0900

    Updated 2023.03.15

---
+++ baekjoon/sorting/p11650.java
@@ -0,0 +1,38 @@
+package baekjoon.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class p11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] nums = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+            nums[i][0] = Integer.parseInt(str[0]);
+            nums[i][1] = Integer.parseInt(str[1]);
+        }
+
+        Arrays.sort(nums, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1];
+            }
+        });
+
+        for (int[] num : nums) {
+            for (int n : num) {
+                sb.append(n + " ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
