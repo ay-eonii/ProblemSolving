cc438596a53acb6c8291e3b2e244084f988dff88
ay-eonii <naayen@naver.com>
2023-04-15 23:33:23 +0900

    Updated 2023.04.15

---
+++ baekjoon/dynamic_programming1/p1904.java
@@ -0,0 +1,25 @@
+package baekjoon.dynamic_programming1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1000001];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
