ba4e1d942a6b5390f96264f477479d2b3571e10f
ay-eonii <naayen@naver.com>
2023-04-20 21:57:07 +0900

    Updated 2023.04.20

---
+++ baekjoon/dynamic_programming1/p1463.java
@@ -0,0 +1,42 @@
+package baekjoon.dynamic_programming1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1463 {
+    private static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(recur(N));
+    }
+
+    private static int recur(int N) {
+        if (dp[N] == null) {
+            if (N % 6 == 0) {
+                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+            } else if (N % 3 == 0) {
+                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+            } else if (N % 2 == 0) {
+                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+            } else {
+                dp[N] = recur(N - 1) + 1;
+            }
+        }
+        return dp[N];
+    }
+}
+
+/*
+* 1. n%3 == 0이면 n/3
+* 2. n%2 == 0이면 n/2
+* 3. n--;
+*
+* 결국 n == 1 이어야 함
+* 3의 배수 | 2의 배수까지만 가면 됨
+* */
