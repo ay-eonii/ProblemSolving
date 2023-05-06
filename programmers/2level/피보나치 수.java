aee24dbce8b5c1ff09bf50f960003118e7b5b3ab
ay-eonii <naayen@naver.com>
2023-05-06 17:06:08 +0900

    Updated 2023.05.06

---
+++ programmers/2level/피보나치 수.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int dp[];
+
+    public int DynamicProgramming(int n) {
+        dp = new int[100001];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+        }
+        return dp[n] % 1234567;
+    }
+}
+
+class Solution {
+    private int dp[];
+
+    public int Recursion(int n) {
+        dp = new int[100001];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        return fib(n);
+    }
+
+    private int fib(int n) {
+        if (n == 0) {
+            return dp[0];
+        }
+        if (n == 1) {
+            return dp[1];
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
