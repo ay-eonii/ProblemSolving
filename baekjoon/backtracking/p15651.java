b9850320511e5a2693457e189d8d64320c6435c2
ay-eonii <naayen@naver.com>
2023-05-14 21:11:48 +0900

    Updated 2023.05.14

---
+++ baekjoon/backtracking/p15651.java
@@ -0,0 +1,41 @@
+package baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p15651 {
+
+    private static StringBuilder sb;
+    private static int[] arr;
+    private static int m;
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        permutation(0);
+        System.out.println(sb);
+    }
+
+    public static void permutation(int r) {
+        if (r == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[r] = i;
+            permutation(r + 1);
+        }
+    }
+}
