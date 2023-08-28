bd128f302d32a4db53ace3925a2d08c29090f3ef
ay-eonii <naayen@naver.com>
2023-08-28 23:47:35 +0900

    Updated 2023.08.28

---
+++ programmers/2level/프로세스.java
@@ -0,0 +1,56 @@
+/* problem
+현재 실행 대기 큐(Queue)에 있는 프로세스의 중요도가 순서대로 담긴 배열 priorities와,
+몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location이 매개변수로 주어질 때,
+해당 프로세스가 몇 번째로 실행되는지 return 하도록 solution 함수를 작성해주세요.
+*/
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Integer> place = new LinkedList<>();
+        Queue<Integer> process = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            process.add(priorities[i]);
+            if (i == location) {
+                place.add(1);
+                continue;
+            }
+            place.add(0);
+        }
+
+        priorities = Arrays.stream(priorities).boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue).toArray();
+
+        int answer = 1;
+        int i = 0;
+        while (!process.isEmpty()) {
+            if (process.peek() == priorities[i]) {
+                if (place.peek() == 1) {
+                    return answer;
+                }
+                process.remove();
+                place.remove();
+                answer++;
+                i++;
+                continue;
+            }
+            process.add(process.poll());
+            place.add(place.poll());
+        }
+
+        return answer;
+    }
+}
+
+/* strategy
+1. Queue 두 개 -> 우선순위, 프로세스: 동시에 움직임
+2. location[] 바탕 프로세스를 Queue<Integer>에서 1(location), 0 로 만들기
+3. location 내림차순 정렬
+4. location[0] == queue.peek() 일때까지 queue 돌리기
+*/
