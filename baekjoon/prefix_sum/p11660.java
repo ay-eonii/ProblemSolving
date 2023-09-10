63fe12e7baf6310c999c46943bf9843762eff933
ay-eonii <naayen@naver.com>
2023-09-10 23:21:17 +0900

    Updated 2023.09.10

---
+++ baekjoon/prefix_sum/p11660.java
@@ -0,0 +1,56 @@
+package baekjoon.prefix_sum;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class p11660 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] colSum = new int[N + 1][N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            colSum[i][1] = Integer.parseInt(st.nextToken());
+            for (int j = 2; j < N + 1; j++) {
+                colSum[i][j] = colSum[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < M; i++) {
+            int answer = 0;
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            while (x1 <= x2) {
+                answer += colSum[x1][y2];
+                answer -= colSum[x1][y1];
+                x1++;
+            }
+            bw.append(String.valueOf(answer)).append("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
+/*
+1. int[][] sum에 열마다 누적합 저장 (가로로 누적합)
+2. y1부터 y2까지 x2열의 값 모두 더하기
+3. y1부터 y2까지 x1 - 1열의 값 모두 빼기
+=> [y1][x2] + ... + [y2][x2] - [y1][x1] + ... + [y2][x1]
+==행 누적합==
+1 3 6 10
+2 5 9 14
+3 7 12 18
+4 9 15 22
+ *  */
