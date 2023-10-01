1e230d60b9612c648cc5db47c64e874b38f11c31
ay-eonii <naayen@naver.com>
2023-10-01 23:42:20 +0900

    Updated 2023.10.01

---
+++ programmers/2level/땅따먹기.java
@@ -0,0 +1,25 @@
+/* problem
+땅따먹기 게임의 땅(land)은 총 N행 4열로 이루어져 있고, 모든 칸에는 점수가 쓰여 있습니다.
+1행부터 땅을 밟으며 한 행씩 내려올 때, 각 행의 4칸 중 한 칸만 밟으면서 내려와야 합니다.
+단, 땅따먹기 게임에는 한 행씩 내려올 때, 같은 열을 연속해서 밟을 수 없는 특수 규칙이 있습니다.
+마지막 행까지 모두 내려왔을 때, 얻을 수 있는 점수의 최대값을 return 하는 solution 함수를 완성해 주세요.
+*/
+
+class Solution {
+    public int solution(int[][] land) {
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][3]);
+            land[i][1] += Math.max(Math.max(land[i - 1][0], land[i - 1][2]), land[i - 1][3]);
+            land[i][2] += Math.max(Math.max(land[i - 1][1], land[i - 1][0]), land[i - 1][3]);
+            land[i][3] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][0]);
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length - 1][i]);
+        }
+
+        return answer;
+    }
+}
