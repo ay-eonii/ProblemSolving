1aa505aef94e6b0f2fbe29de1d65bf9285391dd9
ay-eonii <naayen@naver.com>
2024-04-01 21:48:51 +0900

    Updated 2024.04.01

---
+++ programmers/2level/택배상자.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] order) {
+        int[] priority = new int[order.length];
+        for (int i = 0; i < order.length; i++) {
+            priority[order[i] - 1] = i;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int target = 0;
+        for (int i = 0; i < priority.length; i++) {
+            if (priority[i] == target) {
+                target++;
+            } else {
+                stack.push(priority[i]);
+            }
+            while (!stack.isEmpty() && stack.peek() == target) {
+                stack.pop();
+                target++;
+            }
+        }
+        return target;
+    }
+}
