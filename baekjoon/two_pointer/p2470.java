f996234ca4881fb3e5d6a21cc896c8dcd16bc8cb
ay-eonii <naayen@naver.com>
2024-04-29 13:54:49 +0900

    fix: 오타수정

--- baekjoon/two_pointer/p2470.java
+++ baekjoon/two_pointer/p2470.java
@@ -1,6 +1,5 @@
 /*
-산성 용액과 알칼리성 용액의 특성값이 주어졌을 때,
-이 중 두 개의 서로 다른 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액을 찾는 프로그램을 작성하시오.
+산성 용액과 알칼리성 용액의 특성값이 주어졌을 때, 이 중 두 개의 서로 다른 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액을 찾는 프로그램을 작성하시오.
  */
 
 package baekjoon.two_pointer;
@@ -16,7 +15,7 @@ public class p2470 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st ew StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] solids = new int[n];
         for (int i = 0; i < n; i++) {
             solids[i] = Integer.parseInt(st.nextToken());
