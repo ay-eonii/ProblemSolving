c88c21caef622652647f19906a28ab78006278b6
ay-eonii <naayen@naver.com>
2025-02-14 16:20:42 +0900

    Updated 2025.02.14

---
+++ leetcode/704-Binary Search.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+class BinarySearch {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
+// 오름차순 정렬
+// nums.length <= 10^4
+// => 이분탐색
