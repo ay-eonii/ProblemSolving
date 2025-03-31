6df699ea2e2f1417f3c16b47e4ea8a646ec077fc
ay-eonii <naayen@naver.com>
2025-03-31 13:28:55 +0900

    Updated 2025.03.31

--- baekjoon/graph/p18352.java
+++ baekjoon/graph/p18352.java
@@ -1,63 +1,63 @@
 package baekjoon.graph;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class p18352 {
-    static int[] visited;
-    static ArrayList<Integer>[] A;
-    static int N, M, K, X;
-    static List<Integer> answer;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        N = scan.nextInt();
-        M = scan.nextInt();
-        K = scan.nextInt();
-        X = scan.nextInt();
+    private static int[] dist;
 
-        A = new ArrayList[N + 1];
-        answer = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+        int k = Integer.parseInt(inputs[2]);
+        int x = Integer.parseInt(inputs[3]);
+
+        List<Integer>[] edges = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            inputs = br.readLine().split(" ");
+            edges[Integer.parseInt(inputs[0])].add(Integer.parseInt(inputs[1]));
         }
 
-        for (int i = 0; i < M; i++) {
-            int S = scan.nextInt();
-            int E = scan.nextInt();
-            A[S].add(E);
-        }
+        dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        bfs(edges, x);
 
-        visited = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
-            visited[i] = -1;
-        }
-
-        BFS(X);
-        for (int i = 0; i <= N; i++) {
-            if (visited[i] == K) {
-                answer.add(i);
-            }
-            if (answer.isEmpty()) {
-                System.out.println("-1");
-            } else {
-                Collections.sort(answer);
-                for (int temp : answer) {
-                    System.out.println(temp);
-                }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == k) {
+                sb.append(i).append("\n");
             }
         }
+
+        System.out.print(sb.isEmpty() ? -1 : sb);
     }
 
-    private static void BFS(int Node) {
+    private static void bfs(List<Integer>[] edges, int x) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(Node);
-        visited[Node]++;
+        boolean[] visited = new boolean[edges.length];
+        dist[x] = 0;
+        queue.add(x);
+
         while (!queue.isEmpty()) {
-            int now_Node = queue.poll();
-            for (int i : A[now_Node]) {
-                if (visited[i] == -1) {
-                    visited[i] = visited[now_Node] + 1;
-                    queue.add(i);
+            Integer current = queue.poll();
+            visited[current] = true;
+
+            for (Integer edge : edges[current]) {
+                if (!visited[edge]) {
+                    dist[edge] = Math.min(dist[current] + 1, dist[edge]);
+                    queue.add(edge);
                 }
             }
         }
